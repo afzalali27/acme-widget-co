3c32c22ae24baba38328b9b952f731e7619b87a7
Afzal Ali <afzalali2728@gmail.com>
2025-05-12 17:50:37 +0500

    initialize Basket class

---
+++ basket.rb
@@ -0,0 +1,19 @@
+class Basket
+  PRODUCTS = {
+    'R01': { name: 'Red Widget',   price: 32.95 },
+    'G01': { name: 'Green Widget', price: 24.95 },
+    'B01': { name: 'Blue Widget',  price:  7.95 }
+  }
+
+  DELIVERY_RULES = [
+    { threshold: 90, cost: 0.00 },
+    { threshold: 50, cost: 2.95 },
+    { threshold:  0, cost: 4.95 }
+  ]
+
+  def initialize
+    @items = []
+  end
+
+  
+end
